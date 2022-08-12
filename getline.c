325587604fbd9212611f985766c0f539bc62c512
Mullah <104761945+MullahKer@users.noreply.github.com>
2022-08-12 12:41:31 +0300

    Create getline.c

---
+++ getline.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _getline - gets LINE from environ
+ * @name: pointer to PATH string
+ *
+ * Return: pointer to PATH member string or NULL if not found
+ */
+ istream& getline(istream& ins, MyString& target)
+{
+
+    char ch = 0;
+    istream& get(char& ch);
+    while (ins && ch != '\n')
+    {
+        istream& get(char& ch);
+        target += ch;
+    };
+    return ins;
+};
